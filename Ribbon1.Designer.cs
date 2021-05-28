5a1ff7d243f04b3e0b1d814bb4c64d85f98d6a71
brianash <bash@mechanican.com>
2021-05-28 13:45:25 -0500

    - First commit

---
+++ Ribbon1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ExcelAddIn1
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainTab = this.Factory.CreateRibbonTab();
+            this.SomeName = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.MainTab.SuspendLayout();
+            this.SomeName.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainTab
+            // 
+            this.MainTab.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.MainTab.Groups.Add(this.SomeName);
+            this.MainTab.Label = "TabAddIns";
+            this.MainTab.Name = "MainTab";
+            // 
+            // SomeName
+            // 
+            this.SomeName.Items.Add(this.button1);
+            this.SomeName.Label = "Actions";
+            this.SomeName.Name = "SomeName";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = global::ExcelAddIn1.Properties.Resources.snowWhiteLobster;
+            this.button1.Label = "Run It Billy";
+            this.button1.Name = "button1";
+            this.button1.ScreenTip = "Run Brain\'s Code";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.MainTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.MainTab.ResumeLayout(false);
+            this.MainTab.PerformLayout();
+            this.SomeName.ResumeLayout(false);
+            this.SomeName.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab MainTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup SomeName;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Ribbon1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelAddIn1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            //this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(
+            //    this.button1_Click);
+            
+
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            //Globals.ThisAddIn.Application. = true;
+            //actionsPane2.Hide();
+            //actionsPane1.Show();
+            //toggleButton1.Checked = false;
+
+            Microsoft.Office.Interop.Excel.Worksheet activeWorksheet = ((Microsoft.Office.Interop.Excel.Worksheet)Globals.ThisAddIn.Application.ActiveSheet);
+            var rows = activeWorksheet.Rows;
+            Console.WriteLine(rows.Count);
+            //foreach (var row in rows.Value)
+            //{
+            //    var i = row.ToString();
+            //    Console.WriteLine(i);
+            //}
+
+
+            DateTime dt = DateTime.Now;
+            var rng = Globals.ThisAddIn.Application.get_Range("D2");
+            object value = rng.Value2;
+
+            if (value != null)
+            {
+                if (value is double)
+                {
+                    dt = DateTime.FromOADate((double)value);
+                }
+                else
+                {
+                    DateTime.TryParse((string)value, out dt);
+                }
+            }
+
+            rng.Value = dt;
+
+            //System.Windows.Forms.MessageBox.Show(dt.ToString());
+
+            //firstRow.EntireRow.Insert(Microsoft.Office.Interop.Excel.XlInsertShiftDirection.xlShiftDown);
+            //Microsoft.Office.Interop.Excel.Range newFirstRow = activeWorksheet.get_Range("A1");
+            //newFirstRow.Value2 = "This text was added by using code";
+        }
+
+        
+    }
+}
